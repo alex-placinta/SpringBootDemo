72b47102a25054766935f4e4a9c155176f965d07
Deder Georgeta <t.getty_18@yahoo.com>
2020-09-19 14:43:08 +0300

    Rest Endpoint

--- src/main/java/com/sda/spring/service/CompanyService.java
+++ src/main/java/com/sda/spring/service/CompanyService.java
@@ -1,6 +1,13 @@
 package com.sda.spring.service;
 
 
+import com.sda.spring.dto.CompanyCreateDto;
+import com.sda.spring.dto.CompanyInfoDto;
+import java.util.List;
+
 public interface CompanyService {
 
+    CompanyInfoDto create(CompanyCreateDto company);
+
+    List<CompanyInfoDto> getAllCompanies();
 }
